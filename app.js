609e478b22cc83eaf3316275f00a6f70921bd4c7
AdrianRios25 <144917403+AdrianRios2527@users.noreply.github.com>
2023-12-13 14:54:14 +0000

    retoques

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener("scroll", function() {
+    var elementosAnimados = document.querySelectorAll(".elemento-animado");
+
+    elementosAnimados.forEach(function(elemento, index) {
+      if (esElementoVisible(elemento)) {
+        setTimeout(function() {
+          elemento.classList.add("aparecer");
+        }, index * 400); // Ajusta el valor del retraso (en milisegundos) según tus necesidades
+      }
+    });
+  });
+
+  function esElementoVisible(elemento) {
+    var rect = elemento.getBoundingClientRect();
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    return rect.top <= windowHeight - 100;
+  }
